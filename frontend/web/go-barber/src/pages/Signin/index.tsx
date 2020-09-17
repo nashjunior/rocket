178aec0d3601786620ddd3f06d34541f69b054b6
Nash Junior <nashjuniorjj@gmail.com>
2020-09-17 11:33:39 -0300

    added: signin erro, tooltip error

--- frontend/web/go-barber/src/pages/Signin/index.tsx
+++ frontend/web/go-barber/src/pages/Signin/index.tsx
@@ -1,16 +1,39 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
 import { Container, Content, Background } from './styles';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import Logo from '../../assets/logo.svg';
+import { Form } from '@unform/web';
 import Button from '../../components/Button';
 import Input from '../../components/Input';
+import getValidationErrors from '../../utils/getValidationErrors';
+import { FormHandles } from '@unform/core';
+import * as Yup from 'yup';
 
 const SignIn: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      formRef.current?.setErrors({});
+
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .required('Email Obrigatorio')
+          .email('Digite um e-mail valido'),
+        password: Yup.string().min(6, 'No minimo 6 dígitos'),
+      });
+
+      await schema.validate(data, { abortEarly: false });
+    } catch (error) {
+      formRef.current?.setErrors(getValidationErrors(error));
+    }
+  }, []);
+
   return (
     <Container>
       <Content>
         <img src={Logo} alt="GoBarb" />
-        <form action="">
+        <Form onSubmit={handleSubmit} action="">
           <h1>Faca Seu logon</h1>
           <Input
             icon={FiMail}
@@ -30,7 +53,7 @@ const SignIn: React.FC = () => {
           <Button type="submit">Entrar</Button>
 
           <a href="forgot">Esqueci minha Senha</a>
-        </form>
+        </Form>
 
         <a href="">
           <FiLogIn /> Criar conta
