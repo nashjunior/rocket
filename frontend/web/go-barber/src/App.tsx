a7ccd9f21f1e4786cdac6552ba11918d80b39278
Nash Junior <nashjuniorjj@gmail.com>
2020-09-23 11:50:52 -0300

    added: connected login api, contextApi

--- frontend/web/go-barber/src/App.tsx
+++ frontend/web/go-barber/src/App.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import SignIn from './pages/Signin';
 import GlobalStyle from './styles/global';
+import { AuthProvider } from './context/AuthContext';
 
 const App: React.FC = () => {
   return (
     <>
-      <SignIn />
+      <AuthProvider>
+        <SignIn />
+      </AuthProvider>
       <GlobalStyle />
     </>
   );
--- frontend/web/go-barber/src/pages/Signin/index.tsx
+++ frontend/web/go-barber/src/pages/Signin/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useRef } from 'react';
+import React, { useCallback, useContext, useRef } from 'react';
 import { Container, Content, Background } from './styles';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import Logo from '../../assets/logo.svg';
@@ -8,32 +8,44 @@ import Input from '../../components/Input';
 import getValidationErrors from '../../utils/getValidationErrors';
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
+import { AuthContext } from '../../context/AuthContext';
+
+interface SignInFormData {
+  email: string;
+  password: string;
+}
 
 const SignIn: React.FC = () => {
+  const { user, signIn } = useContext(AuthContext);
+
   const formRef = useRef<FormHandles>(null);
 
-  const handleSubmit = useCallback(async (data: object) => {
-    try {
-      formRef.current?.setErrors({});
+  const handleSubmit = useCallback(
+    async (data: SignInFormData) => {
+      try {
+        formRef.current?.setErrors({});
 
-      const schema = Yup.object().shape({
-        email: Yup.string()
-          .required('Email Obrigatorio')
-          .email('Digite um e-mail valido'),
-        password: Yup.string().min(6, 'No minimo 6 dígitos'),
-      });
+        const schema = Yup.object().shape({
+          email: Yup.string()
+            .required('Email Obrigatorio')
+            .email('Digite um e-mail valido'),
+          password: Yup.string().required('Senha Obrigatoria'),
+        });
 
-      await schema.validate(data, { abortEarly: false });
-    } catch (error) {
-      formRef.current?.setErrors(getValidationErrors(error));
-    }
-  }, []);
+        await schema.validate(data, { abortEarly: false });
+        signIn({ email: data.email, password: data.password });
+      } catch (error) {
+        formRef.current?.setErrors(getValidationErrors(error));
+      }
+    },
+    [signIn],
+  );
 
   return (
     <Container>
       <Content>
         <img src={Logo} alt="GoBarb" />
-        <Form onSubmit={handleSubmit} action="">
+        <Form ref={formRef} onSubmit={handleSubmit} action="">
           <h1>Faca Seu logon</h1>
           <Input
             icon={FiMail}
